f36cdcc0cfb4312488fabea176c3972a7e056eb5
Wilson <139755897+WilsonMozgodel@users.noreply.github.com>
2024-08-29 22:03:46 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-CHANNEL_USERNAME = '@textasdo3'  # Основоной админ канал для юзеров
+CHANNEL_USERNAME = '@'  # Основоной админ канал для юзеров
 TOKEN = "7160522873:AAG6xK81Zvq96PMfV59dZGFDLg4i0Af0OXg"  # Токен бота
 PAYMENT_TOKEN = '13459:AAWdM9gvN5GXqXwu2ebpNRP6FodaMIPcHBu' #Токен платежки
-ADMIN_CHANNEL = '@ss22ddqw' #Админ канал для логов выплат
+ADMIN_CHANNEL = '@' #Админ канал для логов выплат
